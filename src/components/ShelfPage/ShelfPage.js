91d979fd83052291714a7f5493a55dd3e457240f
masonleonhart <mason.leonhart@gmail.com>
2021-02-17 15:05:17 -0600

    form connected to reducer

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
+import useReduxStore from '../../hooks/useReduxStore';
+import { useDispatch } from 'react-redux';
 
 function ShelfPage() {
   let [addItemToggle, setAddItemToggle] = useState(false);
+
+  const dispatch = useDispatch();
+  const store = useReduxStore();
 
   const handleSubmit = e => {
     e.preventDefault();
     setAddItemToggle(false);
   };
+
+  const handleDescChange = e => dispatch({ type: 'SET_ITEM_DESC', payload: e.target.value });
+
+  const handleUrlChange = e => dispatch({ type: 'SET_ITEM_URL', payload: e.target.value });
 
   return (
     <div className="container">
@@ -15,10 +24,10 @@ function ShelfPage() {
         <>
           <form onSubmit={handleSubmit}>
             <label htmlFor='input-description'>Item Description: </label>
-            <input type='text' id='input-description' required />
+            <input type='text' id='input-description' value={store.addItemReducer.itemDescription} onChange={handleDescChange} required />
             <span> </span>
             <label htmlFor='input-url'>Item Image Url: </label>
-            <input type='url' id='input-url' required />
+            <input type='url' id='input-url' value={store.addItemReducer.itemUrl} onChange={handleUrlChange} required />
             <button type='submit'>Save</button>
           </form>
         </> : <button onClick={() => setAddItemToggle(true)}>Add Item</button>}
